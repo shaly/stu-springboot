728056beb3daf44ee8dfbb2d32cb516c63529bc8
shaly <283199611@qq.com>
2018-12-22 16:49:23 +0800

    slf4j日志

--- stu-springboot-config-001/src/main/java/com/syf/study/IndexController.java
+++ stu-springboot-config-001/src/main/java/com/syf/study/IndexController.java
@@ -1,14 +1,20 @@
 package com.syf.study;
 
+import org.apache.tomcat.util.http.fileupload.ThresholdingOutputStream;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.syf.study.service.UserService;
+
 //springmvc控制器
 @Controller
 public class IndexController {
+	private Logger logger =LoggerFactory.getLogger(this.getClass());
+	
 	@Autowired
 	private UserService userService;
 
@@ -28,7 +34,11 @@ public class IndexController {
 
 	@RequestMapping("/toIndexPage")
 	public String toIndexPage() {
-		
+		logger.trace("trace 日志...");
+		logger.debug("debug 日志...");
+		logger.info("info 日志...");
+		logger.warn("warn 日志...");
+		logger.error("error 日志...");
 		return "index";
 	}
 
