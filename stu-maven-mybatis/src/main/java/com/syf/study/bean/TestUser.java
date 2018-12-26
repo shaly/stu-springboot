68c84da7a837b8edb9e84969d9595bb73433e1d3
shaly <283199611@qq.com>
2018-12-26 21:51:25 +0800

    整合mybatis

---
+++ stu-maven-mybatis/src/main/java/com/syf/study/bean/TestUser.java
@@ -0,0 +1,60 @@
+package com.syf.study.bean;
+
+import java.util.Date;
+
+public class TestUser {
+     private Integer id;
+	 private String name;
+	 private Integer age;
+	 private String sex;
+	 private Date birthday;
+	 private String classes;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Integer getAge() {
+		return age;
+	}
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+	public String getSex() {
+		return sex;
+	}
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+	public Date getBirthday() {
+		return birthday;
+	}
+	public void setBirthday(Date birthday) {
+		this.birthday = birthday;
+	}
+	public String getClasses() {
+		return classes;
+	}
+	public void setClasses(String classes) {
+		this.classes = classes;
+	}
+	@Override
+	public String toString() {
+		return "TestUser [id=" + id + ", name=" + name + ", age=" + age + ", sex=" + sex + ", birthday=" + birthday
+				+ ", classes=" + classes + "]";
+	}
+	
+	
+	
+	
+	
+	
+	
+}
